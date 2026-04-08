deec7762c874c35497c65f7bc54d0ceb7d473276
ekrkaxx <karthick.k@ericsson.com>
2026-04-08 16:19:41 +0530

    Revert "fix: WebRTC screen share workaround for incoming calls"
    This reverts commit 8008039ec20ec6fc5caf3bd39fe1f39d2f2009bd.

--- src/webrtc_fix.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// Get JS to inject: monitors WebRTC screen share tracks and triggers
-// ICE restart if frames aren't being decoded (works around a Teams
-// renegotiation bug when receiving calls on Linux).
-const char* webrtc_get_screenshare_fix_js();
